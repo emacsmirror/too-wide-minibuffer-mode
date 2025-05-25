cde6da647ecd980644c02bf676923cd952cfeaf3
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-05-25 22:32:28 +0300

    Fix the comment

--- too-wide-minibuffer-mode.el
+++ too-wide-minibuffer-mode.el
@@ -10,7 +10,7 @@
 ;; Keywords: convenience
 
 
-;; This file is part of GNU Emacs.
+;; This file is not part of GNU Emacs.
 
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
